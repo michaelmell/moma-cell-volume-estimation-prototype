d6c39fbe7fe69c64f008f423ccd2467689d9e2fc
Michael Mell <michael.mell@unibas.ch>
2022-03-01 16:34:06 +0100

    Add method read_and_append_csv_files to read and append eported cell stats.

--- data_import.py
+++ data_import.py
@@ -56,3 +56,53 @@ def get_gl_file_lists(csv_file):
 #         print(glob(path))
     
     pass
+
+def read_and_append_csv_files(csv_file):
+    df = pd.read_csv(csv_file)
+#     print(df)
+    cell_stat_names = []
+    mask_names = []
+    prob_map_names = []
+    dataset_index = []
+    
+    df_result = pd.DataFrame()
+    
+    col_headers = df.columns
+    col_values = {}
+    for col_header in col_headers:
+        col_values[col_header] = []
+    
+    for index, row in df.iterrows():
+        path = row.data_path
+        print(f'path: {path}')
+#         subdirs = next(os.walk(path))[1]
+#         print(f'subdirs: {subdirs}')
+        full_path = path
+        dataset_index.append(index)
+#         full_path = os.path.join(path, subpath)
+        cell_stat_name = glob(full_path+'/ExportedCellStats__*')[0]
+#         print(f'cell_stat_name: {cell_stat_name}')
+        df_tmp = pd.read_csv(cell_stat_name, skiprows=4, delimiter=',')
+#         print(f'df_tmp: {df_tmp}')
+#         print(f'cell_stat_name: {cell_stat_name}')
+#         cell_stat_names.append(cell_stat_name)
+        for col_header in col_headers:
+            df_tmp[col_header] = row[col_header]
+#             print(f'col_header: {col_header}')
+            col_values[col_header].append(row[col_header])
+#         print(df_tmp)
+        df_result = df_result.append(df_tmp)
+#     print('mask_names:')
+#     print(mask_names)
+    
+#     df_result = pd.DataFrame({'cell_stats_path': cell_stat_names,
+#                       'mask_image_path': mask_names,
+#                       'probability_map_path': prob_map_names,
+#                       'dataset_index': dataset_index,
+#                      })
+
+#     for col_header in col_headers:
+#         df_result[col_header] = col_values[col_header]
+    df_result.loc[pd.isnull(df_result['type_of_end']), 'type_of_end'] = 'map'
+    
+    return df_result
